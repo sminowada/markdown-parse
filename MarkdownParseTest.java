2cbcc5ceee4f88036972a5373f1f50979d46d925
sminowada <97696723+sminowada@users.noreply.github.com>
2022-01-27 11:18:35 -0800

    junit test added

---
+++ MarkdownParseTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class MarkdownParseTest {
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void subtraction() {
+        assertEquals(10, 20-10);
+    }
+}
